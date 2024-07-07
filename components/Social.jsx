54fb54cef64d30f3f07c464056635d6ef73c58d3
Matias Paredes <tutecapo1985@gmail.com>
2024-07-07 19:27:38 -0300

    services page features

---
+++ components/Social.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/mjParedes" },
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/matias-paredes-m8j5/",
+  },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
